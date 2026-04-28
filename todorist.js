b7d6ec8450e9a33e1eb545907773e86d4dc7725d
arukari0961730 <arukari096@gmail.com>
2026-04-28 18:37:16 +0900

    課題追加機能を実装（入力チェック・一覧表示対応）

--- todorist.js
+++ todorist.js
@@ -4,7 +4,36 @@ const deadlineInput = document.getElementById("deadlineInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 const errorMessage = document.getElementById("errorMessage");
-
+const tasks = [];
 addBtn.addEventListener("click", function () {
-  alert("追加ボタンが押されました");
+  const title = titleInput.value;
+  const description = descriptionInput.value;
+  const deadline = deadlineInput.value;
+
+  if(title === "" || deadline === ""){
+    errorMessage.textContent="課題名と締切日は必須です"
+    return;
+  }
+
+  errorMessage.textContent="";
+
+  const task = {
+    id: Date.now(),
+    title: title,
+    description: description,
+    deadline: deadline,
+    completed: false
+  };
+
+  tasks.push(task);
+
+  const li = document.createElement("li");
+  li.textContent = title + "(" + deadline +")";
+
+  taskList.appendChild(li);
+
+  console.log(tasks);
+  titleInput.value = "";
+descriptionInput.value = "";
+deadlineInput.value = "";
 });
