3f5544fe312950227b66afc6b225af2e3682c0a1
arukari0961730 <arukari096@gmail.com>
2026-04-28 17:45:51 +0900

    javascrpt要素取得

---
+++ todorist.js
@@ -0,0 +1,10 @@
+const titleInput = document.getElementById("titleInput");
+const descriptionInput = document.getElementById("descriptionInput");
+const deadlineInput = document.getElementById("deadlineInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+const errorMessage = document.getElementById("errorMessage");
+
+addBtn.addEventListener("click", function () {
+  alert("追加ボタンが押されました");
+});
